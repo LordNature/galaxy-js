af2d33a7d52b2c8d5b80590b54d151ea04d2660f
LordNature <lordnature.christian@gmail.com>
2016-10-10 22:46:39 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require('./controllers/terminal');
+
+var express = require('express'), stylus = require('stylus');
+var app = express();
+var year = new Date().getFullYear()
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index', {title: 'Home', year: year, uptime: uptime});
+});
+
+app.get('/anime', function (req, res) {
+  res.send('Coming soon <br><a href="/">Go home.</a>');
+});
+
+app.listen(8080, function () {
+  console.log('galaxy-node started on port 8080.');
+});
---
+++ controllers/terminal.js
@@ -0,0 +1,9 @@
+// To-do: make loop every hour/minute
+const exec = require('child_process').exec;
+exec('uptime -p', (error, stdout) => {
+  if (error) {
+    return;
+    console.error(`Terminal.js: ${error}`);
+  }
+  uptime = `${stdout}`;
+});
